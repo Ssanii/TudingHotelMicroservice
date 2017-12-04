75628b528c7d8641c778f7b261ceb0040201e3a7
Ssanii <2547573018@qq.com>
2017-12-04 15:58:28 +0800

    这是一个可以运行的服务中心

--- build.sh
+++ build.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
 #服务名称
-name="tuding/eureka"
+name="tuding/tuding-eureka"
 
-#首先要判断镜像是否存在
-if docker images ${name} |grep ${name}|grep -v grep
+if [ ! -n "$(docker images '$name' |grep '$name')" ];
 then
-#如果存在则删除后创建
-        echo "开始停止并删除原镜像..."
-        pid=$(docker images $name | sed -n "2,1p" | awk '{print $3}')
-        docker stop ${pid} #停止容器
-        docker rm ${pid} #删除容器
-        docker rmi -f ${pid} #强制删除原镜像
-        echo "原镜像已停止并删除..."
+        docker run -d -p 1000:1000 '$name' # images is not exisists will run at daemon immediatly
+else
+        if [ ! -n "$(docker ps -a | grep '$name')" ];
+        then
+                docker run -d -p 1000:1000 '$name'
+        else
+                docker stop $(docker ps -a | grep '$name' | awk '{print $1}') #stop the docker container
+                docker rm $(docker ps -a | grep '$name' | awk '{print $1}') # remove the docker contrainer
+                latest_id=  $(docker images | grep '$name' | grep 'latest' | awk '{print $3}') #get the images id which tags eqauls 'latest'
+                docker rmi -f $(docker images | grep '$name' | grep -v '$latest_id' | awk '{print $3}') #del images by name but latest
+                docker run -d -p 1000:1000 '$name'
+        fi
 fi
-dockers build -t ${name} . #构建新镜像
-docker run -d -p 1000:1000 ${name}
-echo "新镜像已部署完成..."
+
+
