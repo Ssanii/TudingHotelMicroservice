f7fdcac73e8a23b9b8f2c3ef930e65d0800a0297
Ssanii <2547573018@qq.com>
2017-12-05 12:54:46 +0800

    这是一个可以运行的服务中心

--- build.sh
+++ build.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
-
 #服务名称
 name="tuding/tuding-eureka"
-
 if [ ! -n "$(docker images $name |grep $name)" ];
 then
+        echo 'image is not exisist!'
         docker run -d -p 1000:1000 $name # images is not exisists will run at daemon immediatly
 else
         if [ ! -n "$(docker ps -a | grep $name)" ];
         then
+                echo 'container is not exisist!'
                 docker run -d -p 1000:1000 $name
         else
+                echo 'container is exisist,shut down now....'
                 docker stop $(docker ps -a | grep $name | awk '{print $1}') #stop the docker container
                 docker rm $(docker ps -a | grep $name | awk '{print $1}') # remove the docker contrainer
-                latest_id=  $(docker images | grep $name | grep 'latest' | awk '{print $3}') #get the images id which tags eqauls 'latest'
-                docker rmi -f $(docker images | grep $name | grep -v '$latest_id' | awk '{print $3}') #del images by name but latest
+                latest_id=$(docker images | grep $name | grep 'latest' | awk '{print $3}') #get the images id which tags eqauls 'latest'
+                docker rmi -f $(docker images | grep $name | grep -v $latest_id | awk '{print $3}') #del images by name but latest
                 docker run -d -p 1000:1000 $name
+                echo 'container is rebuild now ...'
         fi
 fi
