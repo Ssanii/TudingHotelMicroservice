32521143aa7a5e9a926d229f48a2a5642cf2e1ab
Ssanii <2547573018@qq.com>
2017-12-09 15:09:57 +0800

    网关中心

--- docker_run.sh
+++ docker_run.sh
@@ -7,6 +7,6 @@ expose_port="1001"
 
 docker run -d -p ${main_port}:${expose_port} ${name}
 echo 'container start is completed!'
-firewall-cmd --permanent --zone=public --add-port ${main_port}/tcp
+firewall-cmd --permanent --zone=public --add-port=${main_port}/tcp
 firewall-cmd --reload
 #shell end
